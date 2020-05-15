13b8f9fef1ba080d4695bfba59a335886a367022
CHAHYUNHO <chamym@naver.com>
2020-05-15 15:49:46 +0900

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 
 gulp.task('default', function(done){
-    gulp.src("es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+    // EsLint 실행
+    gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
     
-    //브라우저 소스
-    gulp.src("public/es6/**/*.js")
+    // 노드 소스
+    gulp.src('es6/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest("public/dist"));
+    .pipe(gulp.dest('dist'));
+    
+    // 브라우저 소스
+    gulp.src('public/es6/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('public/dist'));
     done();
 });
